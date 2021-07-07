7ee35904ccd6de2221b3ccb9669fc4e05fde6abc
t-tran <t-tran@dimage.co.jp>
2021-07-07 09:56:17 +0700

    add function update delete search vi dic

--- dictionary/dsvn_dictionary/models.py
+++ dictionary/dsvn_dictionary/models.py
@@ -16,6 +16,7 @@ class Vi_Dictionary(models.Model):
     hiragana_text = models.CharField(max_length=200, blank=True, default='')
     katakana_text = models.CharField(max_length=200, blank=True, default='')
     description = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
